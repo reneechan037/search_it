e06dc1c55fa4abbb2fdab2b563d06d3ce16f81fe
a513091a <a513091a@hotmail.com>
2018-10-14 17:00:46 +0800

    add registration function;

---
+++ src/Controller/CmdRegisterAccount.java
@@ -0,0 +1,19 @@
+package Controller;
+import Model.Manager.Response;
+import Model.Register.RegisterService;
+
+import java.util.Scanner;
+
+public class CmdRegisterAccount implements Command {
+    public CmdRegisterAccount(){
+
+    }
+
+    @Override
+    public void execute(String[] cmdParts) {
+        RegisterService registerService = new RegisterService();
+        Response response = registerService.register(cmdParts[0], cmdParts[1]);
+
+        System.out.println(response.getDescription());
+    }
+}
