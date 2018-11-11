e26ffdbd27b0ff04ab4eb1b40b10f42fd8a617bb
Hugo <hugo444312@gmail.com>
2018-11-12 01:54:30 +0800

    manger function, check user no implement

--- src/Model/user/Manager.java
+++ src/Model/user/Manager.java
@@ -1,14 +1,72 @@
 package Model.user;
 
-public class Manager implements Role{
+import java.util.ArrayList;
+
+import Model.Login.SearchingService;
+import Model.service_plan.ServicePlan;
+import Model.service_plan.ServicePlanStorage;
+
+public class Manager  implements Role{
+	private static final Manager instance = new Manager();
+
 	private Manager m;
+	private SearchingService searchService;
+
+	// public Manager(String u_id, String u_name, String u_pwd)
+	// {
+	// 	super(u_id, u_name, u_pwd);
+	// 	this.searchService = new SearchingService();
+	// }
+
+	public Manager()
+	{
+		this.searchService = new SearchingService();
+	}
+
+
+	public static Manager getInstance()
+	{
+		return instance;
+	}
+
 	@Override
 	public Manager getRole() {
 		return m;
 	}
 
-	public Manager() {
 
+	public void checkUserPlan(User user)
+	{}
+
+	public boolean addPlan(ServicePlan plan)
+	{
+		ArrayList<ServicePlan> plans = ServicePlanStorage.getInstance().getPlans();
+		return plans.add(plan);
+	}
+
+	// public boolean updatePlan(ServicePlan oldPlan, ServicePlan newPlan)
+	// {
+
+	// 	try {
+	// 		ArrayList<ServicePlan> plans = ServicePlanStorage.getInstance().getPlans();
+	// 		int index = plans.indexOf(oldPlan);
+	// 		plans.set(index, newPlan);
+	// 		System.out.print("asdadasdasds"+plans.size());
+	// 		return true;
+	// 	} catch (Exception e) {
+	// 		//TODO: handle exception
+	// 		return false;
+	// 	}
+		
+
+	// }
+
+	public boolean deletePlan(String planId)
+	{
+		ServicePlan plan = searchService.getPlanById(planId).getServicePlan();
+		ArrayList<ServicePlan> plans = ServicePlanStorage.getInstance().getPlans();
+
+		return plans.remove(plan);
 	}
 
 }
